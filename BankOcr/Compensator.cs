07f6b912cfde74a6bdf8a6ac374ea77e4fbc3080
han <yhan@la-francaise.com>
2018-09-07 17:10:36 +0200

    fix build and make tests green

--- BankOcr/Compensator.cs
+++ BankOcr/Compensator.cs
@@ -1,7 +1,25 @@
-﻿
+﻿namespace BankOcr
+{
+    using System.Collections.Generic;
 
-                            candidates.Add(numericAscii.Value);
-                        }
+    internal class Compensator
+    {
+        internal IList<int> Convert(Digit digit)
+        {
+            var candidates = new List<int>();
+            var replaceAscii = '_';
+
+            for (var i = 0; i < digit.Header.Length; i++)
+            {
+                var header = digit.Header.ToCharArray();
+                if (header[i] == ' ')
+                {
+                    header[i] = replaceAscii;
+                    digit.ReplacedHeader = new string(header);
+                    try
+                    {
+                        var numericAscii = digit.GetNumeric();
+                        if (numericAscii.HasValue) candidates.Add(numericAscii.Value);
                     }
                     catch
                     {
@@ -9,6 +27,28 @@
                     }
                 }
             }
+
+            digit.ReplacedHeader = null;
+            var replaceAscii2 = '|';
+            for (var i = 0; i < digit.Bottom.Length; i++)
+            {
+                var bottom = digit.Bottom.ToCharArray();
+                if (bottom[i] == ' ')
+                {
+                    bottom[i] = replaceAscii2;
+                    digit.ReplacedBottom = new string(bottom);
+                    try
+                    {
+                        var numericAscii = digit.GetNumeric();
+                        if (numericAscii.HasValue) candidates.Add(numericAscii.Value);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+            }
+
             return candidates;
         }
     }
